65284fc7d5fe8dec0883d35d3fb4cb3f4b4f28c8
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-04 19:21:42 +0100

    Implement FolderController

--- system/src/main/java/com/codeclan/example/system/controllers/FolderController.java
+++ system/src/main/java/com/codeclan/example/system/controllers/FolderController.java
@@ -1,4 +1,29 @@
 package com.codeclan.example.system.controllers;
 
+import com.codeclan.example.system.models.Folder;
+import com.codeclan.example.system.repository.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class FolderController {
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "/folders")
+    public ResponseEntity<List<Folder>> getAllFolders(){
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity<Optional<Folder>> getFolder(@PathVariable Long id){
+        return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
 }
