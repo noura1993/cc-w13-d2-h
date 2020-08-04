87a8751e525420b1907a729d76c36e6d26b305f2
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-08-04 18:52:31 +0100

    Implement Folder class and FolderRepository

--- system/src/main/java/com/codeclan/example/system/models/Folder.java
+++ system/src/main/java/com/codeclan/example/system/models/Folder.java
@@ -1,4 +1,67 @@
 package com.codeclan.example.system.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folder")
+
 public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnoreProperties({"folders"})
+    private User user;
+
+    private List<File> files;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.user = user;
+        this.files = new ArrayList<File>();
+    }
+
+    public Folder() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
 }
--- system/src/main/java/com/codeclan/example/system/repository/FolderRepository.java
+++ system/src/main/java/com/codeclan/example/system/repository/FolderRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.example.system.repository;
 
-public interface FolderRepository {
+import com.codeclan.example.system.models.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FolderRepository extends JpaRepository<Folder, Long> {
 }
